3c38736beb6afde0bd59ad981581d489f3f60543
Toshiki Takakubo <219tosiki@gmail.com>
2024-01-04 17:38:38 +0900

    Refactor: PrefectureList component to use custom hooks

--- src/components/prefectures/PrefectureList.tsx
+++ src/components/prefectures/PrefectureList.tsx
@@ -1,16 +1,14 @@
 "use client";
-import React, { useCallback, useEffect, useState } from "react";
-import { fetchPrefectures } from "@/api/fetchPrefectures";
-import { Prefecture } from "@/types/PrefectureTypes";
+import React, { useState } from "react";
 import Checkbox from "@/components/prefectures/Checkbox";
-import { fetchPopulation } from "@/api/fetchPopulation";
 import Graph from "@/components/graph/Graph";
-import { GraphData } from "@/types/GraphTypes";
+import usePrefectures from "@/hooks/usePrefectures";
+import useGraphData from "@/hooks/useGraphData";
 
 const PrefectureList = () => {
-  const [prefs, setPrefs] = useState<Prefecture[]>([]);
   const [selectedPrefs, setSelectedPrefs] = useState<number[]>([]);
-  const [allData, setAllData] = useState<GraphData["dataList"]>([]);
+  const { prefs } = usePrefectures();
+  const { graphData } = useGraphData(selectedPrefs, prefs);
 
   const handleCheckboxChange = (prefCode: number, isChecked: boolean): void => {
     setSelectedPrefs((prevSelectedPrefs) => {
@@ -21,41 +19,6 @@ const PrefectureList = () => {
       }
     });
   };
-
-  const handleGraphUpdate = useCallback(async () => {
-    try {
-      const allDataPromises = selectedPrefs.map(async (prefCode) => {
-        const response = await fetchPopulation(String(prefCode));
-        const pref = prefs.find((pref) => pref.prefCode === prefCode);
-        return {
-          prefName: pref ? pref.prefName : "non-existent",
-          data: response.result.data[0].data,
-        };
-      });
-
-      const newData = await Promise.all(allDataPromises);
-      setAllData(newData);
-    } catch (error) {
-      console.error("Failed to fetch data", error);
-    }
-  }, [selectedPrefs, prefs]);
-
-  useEffect(() => {
-    handleGraphUpdate();
-  }, [handleGraphUpdate]);
-
-  useEffect(() => {
-    const getPrefectures = async () => {
-      try {
-        const response = await fetchPrefectures();
-        setPrefs(response.result);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    getPrefectures();
-  }, []);
 
   return (
     <div>
@@ -70,7 +33,7 @@ const PrefectureList = () => {
           />
         ))}
       </div>
-      <Graph dataList={allData} />
+      <Graph dataList={graphData} />
     </div>
   );
 };
