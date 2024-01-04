8fddb53d710f25e2519903d3b59181af1692b5db
Toshiki Takakubo <219tosiki@gmail.com>
2024-01-04 15:46:56 +0900

    Add: Calling Graph Components

--- src/components/prefectures/PrefectureList.tsx
+++ src/components/prefectures/PrefectureList.tsx
@@ -1,12 +1,48 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { fetchPrefectures } from "@/api/fetchPrefectures";
 import { Prefecture } from "@/types/PrefectureTypes";
 import Checkbox from "@/components/prefectures/Checkbox";
+import { fetchPopulation } from "@/api/fetchPopulation";
+import Graph from "@/components/graph/Graph";
+import { GraphData } from "@/types/GraphTypes";
 
 const PrefectureList = () => {
   const [prefs, setPrefs] = useState<Prefecture[]>([]);
   const [selectedPrefs, setSelectedPrefs] = useState<number[]>([]);
+  const [allData, setAllData] = useState<GraphData["dataList"]>([]);
+
+  const handleCheckboxChange = (prefCode: number, isChecked: boolean): void => {
+    setSelectedPrefs((prevSelectedPrefs) => {
+      if (isChecked) {
+        return [...prevSelectedPrefs, prefCode];
+      } else {
+        return prevSelectedPrefs.filter((code) => code !== prefCode);
+      }
+    });
+  };
+
+  const handleGraphUpdate = useCallback(async () => {
+    try {
+      const allDataPromises = selectedPrefs.map(async (prefCode) => {
+        const response = await fetchPopulation(String(prefCode));
+        const pref = prefs.find((pref) => pref.prefCode === prefCode);
+        return {
+          prefName: pref ? pref.prefName : "non-existent",
+          data: response.result.data[0].data,
+        };
+      });
+
+      const newData = await Promise.all(allDataPromises);
+      setAllData(newData);
+    } catch (error) {
+      console.error("Failed to fetch data", error);
+    }
+  }, [selectedPrefs, prefs]);
+
+  useEffect(() => {
+    handleGraphUpdate();
+  }, [handleGraphUpdate]);
 
   useEffect(() => {
     const getPrefectures = async () => {
@@ -21,16 +57,6 @@ const PrefectureList = () => {
     getPrefectures();
   }, []);
 
-  const handleCheckboxChange = (prefCode: number, isChecked: boolean): void => {
-    setSelectedPrefs((prevSelectedPrefs) => {
-      if (isChecked) {
-        return [...prevSelectedPrefs, prefCode];
-      } else {
-        return prevSelectedPrefs.filter((code) => code !== prefCode);
-      }
-    });
-  };
-
   return (
     <div>
       <div>
@@ -44,7 +70,7 @@ const PrefectureList = () => {
           />
         ))}
       </div>
-      <div>{selectedPrefs.map((prefCode) => prefCode + " ")}</div>
+      <Graph dataList={allData} />
     </div>
   );
 };
