4ed238ddbb3e7884d39e6819edc83a6e74d15aa6
Toshiki Takakubo <219tosiki@gmail.com>
2024-01-04 17:38:03 +0900

    Add: useGraphData hook for fetching and managing graph data

---
+++ src/hooks/useGraphData.ts
@@ -0,0 +1,45 @@
+// hooks/usePopulationData.ts
+import { useState, useEffect } from "react";
+import { fetchPopulation } from "@/api/fetchPopulation";
+import { Prefecture } from "@/types/PrefectureTypes";
+import { GraphData } from "@/types/GraphTypes";
+
+const useGraphData = (selectedPrefs: number[], prefs: Prefecture[]) => {
+  const [graphData, setGraphData] = useState<GraphData[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const getPopulationData = async () => {
+      try {
+        const dataPromises = selectedPrefs.map(async (prefCode) => {
+          const response = await fetchPopulation(String(prefCode));
+          const pref = prefs.find((p) => p.prefCode === prefCode);
+          return {
+            prefName: pref ? pref.prefName : "non-existent",
+            data: response.result.data[0].data,
+          };
+        });
+
+        const newData = await Promise.all(dataPromises);
+        setGraphData(newData);
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err);
+        } else {
+          setError(new Error("Unknown error occurred"));
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (selectedPrefs.length >= 0) {
+      getPopulationData();
+    }
+  }, [selectedPrefs, prefs]);
+
+  return { graphData, loading, error };
+};
+
+export default useGraphData;
