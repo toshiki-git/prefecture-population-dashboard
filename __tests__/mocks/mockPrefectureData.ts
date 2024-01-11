eda4cab3cb89f6c6c9937307a2e6d891873c1996
Toshiki Takakubo <219tosiki@gmail.com>
2024-01-11 12:47:56 +0900

    Add: tests for PrefectureList component

---
+++ __tests__/mocks/mockPrefectureData.ts
@@ -0,0 +1,8 @@
+import { Prefecture } from "@/types/PrefectureTypes";
+
+export const mockPrefectureData: Prefecture[] = [
+  { prefName: "北海道", prefCode: 1 },
+  { prefName: "青森県", prefCode: 2 },
+];
+
+export default mockPrefectureData;
